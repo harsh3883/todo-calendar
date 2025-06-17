0294e5c60a67c155be5fe9b7baecc2d7ef850919
harsh3883 <122553725+harsh3883@users.noreply.github.com>
2025-06-17 16:32:56 +0530

    Updated views.py with comments

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -10,18 +10,18 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     
-    def create(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):  #POST request
         logger.info(f"Creating todo with data: {request.data}")
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)   #initialize with incoming datat
         if serializer.is_valid():
-            todo = serializer.save()
+            todo = serializer.save()    #Save todo 
             logger.info(f"Todo created successfully: {todo}")
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)    # send response
         else:
             logger.error(f"Validation errors: {serializer.errors}")
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   #send error response
     
-    def update(self, request, *args, **kwargs):
+    def update(self, request, *args, **kwargs):   #PUT/POST request
         logger.info(f"Updating todo {kwargs.get('pk')} with data: {request.data}")
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
@@ -34,9 +34,9 @@ class TodoViewSet(viewsets.ModelViewSet):
             logger.error(f"Validation errors: {serializer.errors}")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def destroy(self, request, *args, **kwargs):
+    def destroy(self, request, *args, **kwargs):    #DELETE REQUEST
         logger.info(f"Deleting todo {kwargs.get('pk')}")
-        instance = self.get_object()
-        self.perform_destroy(instance)
+        instance = self.get_object()   #find the id of todo
+        self.perform_destroy(instance)  #delete it
         logger.info("Todo deleted successfully")
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)  #send response
